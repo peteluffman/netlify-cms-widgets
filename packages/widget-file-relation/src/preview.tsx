6dac45a741357a6eeecfe68a85f1ad22ceef7b45
Pete Luffman <studio@peteluffman.com>
2019-09-04 15:00:29 +0100

    [relation] Fixed bug where error would be thrown on removing item

--- packages/widget-file-relation/src/preview.tsx
+++ packages/widget-file-relation/src/preview.tsx
@@ -2,7 +2,11 @@ import * as React from 'react'
 
 export const Preview = ({ value }) => {
   const display = value 
-    ? value.map(item => item.get('label')).join(', ') 
+    ? value.map(item => {
+      if( !item ) return null;
+      const label =  item.get('label')
+      return Array.isArray( label ) ? label.join(', ') : label
+    }) 
     : ''
   return <p>{display}</p>
 }
